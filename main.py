cd2f8e1a53fef753ec03948148ff542f56d3ae43
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-11-03 04:13:21 -0400

    elif condition

---
+++ main.py
@@ -0,0 +1,9 @@
+n= float(input("inter number : "))
+if n >=0 and   n <= 10:
+    print("F")
+elif n >10 and  n <= 12:
+    print("C")
+elif n >12 and n <= 20:
+    print("P")
+else:
+    print("Out of reng")
