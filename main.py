7fd229b7e94a23dbaae341a878949e23cf3410a2
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-11-03 03:55:36 -0400

    if condition

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+# x= input("pleas inter first number :")
+# y= input("plleas inter second number : ")
+#
+# try:
+#     print(int(x) + int(y))
+#     raise ValueError ("value of input isnt number")
+# except:
+#     print("value of input isnt number")
+
+n= float(input("inter number : "))
+if n >=0:
+    print("P")
+else:
+    print("N")
