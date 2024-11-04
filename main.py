97291b690b3f5d0aed0a5d88375bbba552adf9c6
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-11-04 00:10:32 -0400

    largest_number

---
+++ main.py
@@ -0,0 +1,12 @@
+x=float(input("inter number 1 x : "))
+y=float(input("inter number 2 y : "))
+z=float(input("inter number 3 z : "))
+
+
+if x> y and x > z:
+    print("X : ",x)
+elif y > x and y > z:
+    print("Y :", y)
+elif z > x and z > y :
+    print("Z : ",z)
+
