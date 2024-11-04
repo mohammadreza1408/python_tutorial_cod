54bd22136875c944b0d7cfa88d88986e3b95b664
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-11-04 04:22:28 -0400

    Max Numbers

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+z=1
+
+i =0
+_max = 0
+while True:
+    x = int(input(f"inter your number {z}: "))
+    if x == 0:
+        print("max is : ", _max)
+        exit()
+    else:
+        if x >_max:
+            _max = x
+    z+=1
+
+
+
+
+
