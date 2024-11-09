52ea1b056ad97c73c508ed0ab9ad52697be461cf
mohammad <127003546+mohammadreza1408@users.noreply.github.com>
2024-11-09 02:12:28 -0400

    Tuple,Set

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+
+x=2,5.8,True,"salam"
+
+print(type(x))
+for i in x:
+    print(i)
+
+print(x[0])
+
+print("**********")
+y= {"ali",6,5.9,"salam",6,"salam"}
+print(type(y))
+print(y)
+for i in y:
+    print(i)
+
+y.add(56)
+print(y)
+y.clear()
+print("set : ",y)
+y.pop()
+print(y)
+
